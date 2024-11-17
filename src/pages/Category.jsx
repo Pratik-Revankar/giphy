c21f9536cc009cf52f91da679eb6f2baa2d090c8
Pratik <pratikrevankar0209@gmail.com>
2024-11-17 21:29:38 +0530

    add search by category feature

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { GifState } from "../context/context";
+import Gif from "../components/Gif";
+import FollowOn from "../components/FollowOn";
 
 const Category = () => {
-  return (
-    <div>Category</div>
-  )
-}
+  const [results, setResults] = useState([]);
+  const { category } = useParams();
+  const { gf } = GifState();
 
-export default Category
+  const fetchResults = async () => {
+    const { data } = await gf.gifs(category, category);
+
+    setResults(data);
+  };
+
+  useEffect(() => {
+    fetchResults();
+  }, [category]);
+  return <div className="flex flex-col sm:flex-row gap-5 my-4">
+    <div className="w-full sm:w-72">
+      {results.length>0 && <Gif  gif={results[0]}/>}
+      <span className="text-gray-400 text-sm pt-2">
+        Don&apos;t tell it to me, GIF it to me!
+      </span>
+      <FollowOn/>
+
+      <div className="divider"></div>
+    </div>
+
+    <div>
+      <h2 className="text-4xl pb-1 font-extralight capitalize">{category.split("-").join(" & ")} GIFs</h2>
+      <h2 className="text-lg text-gray-400 pb-3 font-bold hover:text-gray-50 cursor-pointer">
+        @{category}
+      </h2>
+
+      {results.length > 0 && (
+        <div className="columns-2 md:columns-3 lg:columns-4 xl:columns-5 2xl:columns-6 gap-2">
+          {results.slice(1).map((gif) => (
+            <Gif gif={gif} key={gif.id} />
+          ))}
+        </div>
+      ) }
+    </div>
+    
+  </div>;
+};
+
+export default Category;
