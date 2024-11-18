8dde27665c0fc0a746bd08ed6f69767a11172f03
Pratik <pratikrevankar0209@gmail.com>
2024-11-18 11:53:43 +0530

    add to favorites feature

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -1,19 +1,41 @@
 import { GiphyFetch } from "@giphy/js-fetch-api";
-import {  createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
-const GifContext = createContext()
+const GifContext = createContext();
 
-const GifProvider =({children})=>{
-    const [gifs,setGifs] = useState([])
-    const [filter,setFilter]=useState('gifs')
-    const [favorites,setFavorites]=useState([])
+const GifProvider = ({ children }) => {
+  const [gifs, setGifs] = useState([]);
+  const [filter, setFilter] = useState("gifs");
+  const [favorites, setFavorites] = useState([]);
 
-    console.log(import.meta.env.VITE_GIPHY_KEY)
-    const gf = new GiphyFetch(import.meta.env.VITE_GIPHY_KEY)
-    return <GifContext.Provider value={{gf,gifs,setGifs,filter,setFilter,favorites}}>{children}</GifContext.Provider>
-}
+  const addToFavorites = (id) => {
+    if (favorites.includes(id)) {
+      const updatedFavorites = favorites.filter((itemId) => itemId !== id);
+      localStorage.setItem("favoriteGIFs", JSON.stringify(updatedFavorites));
+      setFavorites(updatedFavorites);
+    } else {
+      const updatedFavorites = [...favorites];
+      updatedFavorites.push(id);
+      localStorage.setItem("favoriteGIFs", JSON.stringify(updatedFavorites));
+      setFavorites(updatedFavorites);
+    }
+  };
 
-export const GifState =()=>{
-    return useContext(GifContext)
-}
-export default GifProvider
+  useEffect(() => {
+    const favorites = JSON.parse(localStorage.getItem("favoriteGIFs")) || [];
+    setFavorites(favorites);
+  },[]);
+  const gf = new GiphyFetch(import.meta.env.VITE_GIPHY_KEY);
+  return (
+    <GifContext.Provider
+      value={{ gf, gifs, setGifs, filter, setFilter, favorites ,addToFavorites }}
+    >
+      {children}
+    </GifContext.Provider>
+  );
+};
+
+export const GifState = () => {
+  return useContext(GifContext);
+};
+export default GifProvider;
